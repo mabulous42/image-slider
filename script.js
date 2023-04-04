83129dfeedd7639513f95e97b7e263f9bbdd87ef
Abbas <tundeeey@gmail.com>
2023-04-04 10:07:14 +0100

    I added two more pictures

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 let display = document.getElementById("display");
-// let img1 = document.getElementById("img1");
-// let img2 = document.getElementById("img2");
-// let img3 = document.getElementById("img3");
-// let imgDiv = document.getElementById("show-img");
 
 let img1 = document.createElement("img");
 img1.src = "Images/image1.jpeg";
@@ -13,21 +9,18 @@ img2.src = "Images/image2.jpeg";
 let img3 = document.createElement("img");
 img3.src = "Images/image3.jpeg";
 
-// document.body.appendChild(img1);
+let img4 = document.createElement("img");
+img4.src = "Images/img4.jpeg";
 
-let arrImage = [img1, img2, img3];
-// display.appendChild(arrImage[0]).style.display = "none";
-// arrImage.push(img2);
-// display.appendChild(arrImage[1]);
+let img5 = document.createElement("img");
+img5.src = "Images/img5.JPG";
 
-// for (let index = 0; index < arrImage.length; index++) {
-//     display = arrImage[index].style.display = "none";
-// }
+
+let arrImage = [img1, img2, img3, img4, img5];
 
 let i = 0
 display.appendChild(arrImage[i]);
 
-// console.log(arrImage.length);
 function showPrevious() {  
     if (i === 0) {
         i = 0;
@@ -38,17 +31,21 @@ function showPrevious() {
         display.appendChild(arrImage[i]).style.display = "block";
     }   
 }
-// setInterval(showPrevious(), 10000)
-function showNext() {
+
+setInterval(() => {
     if (i < arrImage.length-1) {
         display.appendChild(arrImage[i]).style.display = "none";   
         i++;
         display.appendChild(arrImage[i]).style.display = "block";
-    }     
-    else {
-        i = arrImage.length-1;
+    }
+}, 5000);
+
+function showNext() {
+    if (i > arrImage.length-1) {
+        display.appendChild(arrImage[i]).style.display = "none";   
+        i++;
+        display.appendChild(arrImage[i]).style.display = "block";
     }
 }
-// setInterval(showNext(), 10000)
 
 
